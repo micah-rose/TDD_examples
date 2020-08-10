c204ae9053ede0a6a9ab9cb94431401fee6267ff
Micah Lund <micah.r.lund@gmail.com>
2020-08-10 15:50:00 -0600

    Wrote first test case for calling fizzbuzz.

--- fizzbuzz/fizzbuzz.js
+++ fizzbuzz/fizzbuzz.js
@@ -3,4 +3,4 @@
 // Get "2" when 2 is passed in
 // Get "Fizz" when 3 is passed in
 // Get "Buzz" when 5 is passed in
-// Get "FizzBuzz" when 15
+// Get "FizzBuzz" when 15 is passed in, a multiple of 3 and 5
