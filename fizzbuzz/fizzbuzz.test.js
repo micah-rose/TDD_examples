79e1f283b93de70da164f638f83186e0f0616bf5
Micah Lund <micah.r.lund@gmail.com>
2020-08-10 16:01:08 -0600

    Wrote second failed test for return a 1 when a 1 is passed in as the argument.

--- fizzbuzz/fizzbuzz.test.js
+++ fizzbuzz/fizzbuzz.test.js
@@ -4,3 +4,8 @@ var expect =  require('chai').expect;
 it('can call fizzBuzz', function () {
     fizzBuzz(1);
 })
+
+it('returns 1 with 1 as the argument', function () {
+    var result = fizzBuzz(1);
+    expect(result).to.equal('1');
+})
