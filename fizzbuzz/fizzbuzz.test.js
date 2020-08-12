6f996f4b2ad145dde78006c7e134869966a64e0c
Micah Lund <micah.r.lund@gmail.com>
2020-08-12 16:36:25 -0600

    Refactored code to remove duplicate calls to fizzBuzz.

--- fizzbuzz/fizzbuzz.test.js
+++ fizzbuzz/fizzbuzz.test.js
@@ -1,10 +1,6 @@
 var expect =  require('chai').expect;
 const fizzBuzz = require('./fizzbuzz');
 
-it('can call fizzBuzz', function () {
-    fizzBuzz(1);
-})
-
 it('returns 1 with 1 as the argument', function () {
     var result = fizzBuzz(1);
     expect(result).to.equal('1');
