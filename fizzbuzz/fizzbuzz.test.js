31453497c7b10255ff390ba14cec75983e716052
Micah Lund <micah.r.lund@gmail.com>
2020-08-12 16:43:10 -0600

    Added a test function to streamline the use of fizzBuzz.

--- fizzbuzz/fizzbuzz.test.js
+++ fizzbuzz/fizzbuzz.test.js
@@ -1,7 +1,20 @@
+// Phases for TDD 
+// 1. Red phase - failing test 
+// 2. Green phase - passing test 
+// 3. Refactor phase - refactor code to clean it up
+
 var expect =  require('chai').expect;
 const fizzBuzz = require('./fizzbuzz');
 
+function checkFizzBuzz(testValue, expectedResult){
+    var result = fizzBuzz(testValue);
+    expect(result).to.equal(expectedResult);
+}
+
 it('returns 1 with 1 as the argument', function () {
-    var result = fizzBuzz(1);
-    expect(result).to.equal('1');
+   checkFizzBuzz(1, '1');
+})
+
+it('returns 2 with 2 as the argument', function () {
+    checkFizzBuzz(2, '2');
 })
