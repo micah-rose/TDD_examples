b365a11c13983ec065136f178845ace910e2f384
Micah Lund <micah.r.lund@gmail.com>
2020-08-13 13:25:17 -0600

    Refactored test file to call addItemPrice in the beforeEach.

--- supermarket/supermarket.test.js
+++ supermarket/supermarket.test.js
@@ -3,17 +3,16 @@ const Checkout = require('./supermarket');
 
 beforeEach(function(){
     checkout = new Checkout();
+    checkout.addItemPrice('a', 1);
+    checkout.addItemPrice('b', 2);
 })
 
 it('Can calculate current total', function(){
-    checkout.addItemPrice('a', 1);
     checkout.addItem('a');
     expect(checkout.calculateTotal()).to.equal(1);
 })
 
 it('Can add multiple items and get correct total', function() {
-    checkout.addItemPrice('a', 1);
-    checkout.addItemPrice('b', 2);
     checkout.addItem('a');
     checkout.addItem('b');
     expect(checkout.calculateTotal()).to.equal(3);
