d0f839dd2888f9147f19a3e19506998824ecf295
Micah Lund <micah.r.lund@gmail.com>
2020-08-13 13:11:29 -0600

    Wrote failed test for adding multiple items and getting the correct total.

--- supermarket/supermarket.test.js
+++ supermarket/supermarket.test.js
@@ -10,3 +10,11 @@ it('Can calculate current total', function(){
     checkout.addItem('a');
     expect(checkout.calculateTotal()).to.equal(1);
 })
+
+it('Can add multiple items and get correct total', function() {
+    checkout.addItemPrice('a', 1);
+    checkout.addItemPrice('b', 2);
+    checkout.addItem('a');
+    checkout.addItem('b');
+    expect(checkout.calculateTotal()).to.equal(3);
+})
